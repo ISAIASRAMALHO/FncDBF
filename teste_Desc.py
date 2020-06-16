460f601c3d568c05c98511ffe840c289d6cfed41
ISAIASRAMALHO <isaiasramalhoepai@gmail.com>
2020-06-15 22:20:06 -0300

    NaoDesista

--- teste_Desc.py
+++ teste_Desc.py
@@ -5,13 +5,13 @@ def descript( v ):
     formula = 0
 
     for i, c in enumerate( v ):
-        convert_text = ord( c ) - 22 * (i+1)
-        if convert_text < 0:
-            caractere = ( chr( convert_text + 256 ) )
+        convert_text = ord( c ) - ( 22 * (i+1) ) 
+        if convert_text >= 0 and convert_text <= 255:
+            caractere = ( chr( convert_text  ) )
+        elif convert_text < 0:
+            caractere = ( chr( convert_text + 255  ) )
         elif convert_text > 255:
-            caractere = ( chr( convert_text - 255  ) )
-        else:
-            caractere= ( chr( convert_text ) )
+            caractere= ( chr( convert_text ) - 255 )
         r += caractere
         print( 'Ordem: {} - Ordinal: {} - Caractere Original: {} - Formula ( 22 * ({}+1): {} - Caractere Descriptografado: {}'
             .format( i, ord(c), c, i,  int(22 * (i+1)), caractere))
@@ -23,11 +23,14 @@ def descript( v ):
 # CAMPO C_CODIGO = 00002. C_CLIENTE = CARLOS JOSE ALVES FEITOSA
 word = 'Ym¤½×ºú/7(_ ¥É¬èý8Icg\r´Êàö"8Ndz¦¼Òèþ'
 
+# for i, c in enumerate( word ):
+#     print('Ordem: {} - Caractere: {} - Ordinal: {}'.format( i, c, ord( c )))
+
 w2 = '/7(_ ¥É'
 
 word2 = 'lm³Òãü+@(l¥É¬õ6?cF\r´Êàö"8Ndz¦¼Òèþ'
 
-print( descript(word) )
+print( descript(word2) )
 
 # w1 = word[0:8]
 # w2 = word[9:17]
