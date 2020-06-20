8e354d27369ca135f087511fa4a7ec94c34d4b7d
ISAIASRAMALHO <isaiasramalhoepai@gmail.com>
2020-06-20 20:55:31 -0300

    Persistindo

--- teste_Desc.py
+++ teste_Desc.py
@@ -8,6 +8,10 @@ def descript( v ):
     particao = []
     cont = 0
     tam = len( v )
+    r = ''
+    caractere = ''
+    n = 0
+    convert_text = 0
     # ARREDONDANDO...
     if tam % 8 > 0:
         d = int( tam / 8 ) + 1
@@ -17,31 +21,23 @@ def descript( v ):
     for q in range( 0, d):
         particao.append( v[ cont:cont+8] )
         cont += 8
-    
-    # DESCRIPTOGRAFANDO POR PARTES.
-
-    r = ''
-    caractere = ''
-    n = 0
-    convert_text = 0
-
-
-    # for i, c in enumerate( v[9:17] ):
-    #     convert_text = ord( c  ) - ( 22 * ( i +1 ) )
-    #     if convert_text >= 0 and convert_text <= 255:
-    #         caractere = ( chr( convert_text ) )
-    #     elif convert_text < 0:
-    #         caractere = ( chr( convert_text + 255 ) )
-    #     elif convert_text > 255:
-    #         caractere = ( chr( convert_text - 255 ) )
-    #     r += caractere
-    #     print( 'Ordinal {}, Caractere {}'.format( convert_text, caractere))
+        
+        # DESCRIPTOGRAFANDO POR PARTES.
+        for i, c in enumerate( particao[q] ):
+            for it in c:
+                if ord(it) < ( 22 * (i+1)):
+                    convert_text = ( 22 * ( i +1 ) ) - ord( it  )
+                elif ord(it) > ( 22 * (i+1)):
+                    convert_text = ord( it  ) - ( 22 * ( i +1 ) )
+                caractere = ( chr( convert_text ) )
+                r += caractere
+            print( 'Ordem: {}, Código ASCII Original: {}, Fórmula ( 22 * ( {} + 1 ) ): {}, Novo Código ASCII: {}'.format( i, it, i, convert_text, caractere))
     return r       
 
 
 
 
 # CAMPO C_CODIGO = 00002. C_CLIENTE = CARLOS JOSE ALVES FEITOSA
-word = 'Ym¤½×ºú/7(_ ¥É¬èý8Icg\r´Êàö"8Ndz¦¼Òèþ'
+word = 'Ym¤½×ºú/8(_ ¥É¬èý8Icg\r´Êàö"8Ndz¦¼Òèþ'
 
 print( descript(word ) )
