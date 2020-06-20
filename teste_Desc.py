3e9de9fff4ef5994d61d6bee741faf8a556adcec
ISAIAS RAMALHO <60304236+ISAIASRAMALHO@users.noreply.github.com>
2020-06-20 14:35:34 -0300

    Update teste_Desc.py

--- teste_Desc.py
+++ teste_Desc.py
@@ -1,23 +1,41 @@
+# VAMOR RECOMEÇAR.
+# PARTICIONANDO A STRING DE CARACTERES.
+
+
+
 def descript( v ):
-    formula = 0
-    t = len( v )
+    # PARTICIONANDO O PARAMENTRO "v"
+    particao = []
+    cont = 0
+    tam = len( v )
+    # ARREDONDANDO...
+    if tam % 8 > 0:
+        d = int( tam / 8 ) + 1
+    else:
+        d = int( tam / 8 )
+    # PARTICIONANDO
+    for q in range( 0, d):
+        particao.append( v[ cont:cont+8] )
+        cont += 8
+    
+    # DESCRIPTOGRAFANDO POR PARTES.
+
     r = ''
     caractere = ''
     n = 0
     convert_text = 0
-    # for i in range(0, t ):
-    #     formula = ord( v[i] ) - ( 22 * (i + 1) )
-    #     caractere = chr( formula )
-    #     print( 'Ordinal: {} - Calculo {} - Resultado {} - Caractere {}'
-    #             .format( ord( v[i]), (22 * (i+1)), formula, caractere))
+
+
+    # for i, c in enumerate( v[9:17] ):
+    #     convert_text = ord( c  ) - ( 22 * ( i +1 ) )
+    #     if convert_text >= 0 and convert_text <= 255:
+    #         caractere = ( chr( convert_text ) )
+    #     elif convert_text < 0:
+    #         caractere = ( chr( convert_text + 255 ) )
+    #     elif convert_text > 255:
+    #         caractere = ( chr( convert_text - 255 ) )
     #     r += caractere
-    for i, c in enumerate( v ):
-        # if i > 9: n =0
-        convert_text = ord( c )
-        caractere = ( chr( convert_text - 22 * (i +1) ) )
-        # n += 1
-        r += caractere
-        print( 'Ordinal {}, Caractere {}'.format( convert_text, caractere))
+    #     print( 'Ordinal {}, Caractere {}'.format( convert_text, caractere))
     return r       
 
 
