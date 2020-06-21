4501d7941b1447d3fa8271bb15264ef0163e52e6
ISAIASRAMALHO <isaiasramalhoepai@gmail.com>
2020-06-20 21:05:10 -0300

    Msg

--- teste_Desc.py
+++ teste_Desc.py
@@ -3,25 +3,17 @@ def descript( v ):
     caractere = ''
     contador, convert_text = 0, 0
     for i, c in enumerate( v ):
-        if i < 9:
-            convert_text = ord( c ) - ( 22 * ( contador +1 ) )
-            contador +=1
-        elif i > 9:
-            contador = 0  
-        # if convert_text >= 0 and convert_text <= 255:
-        #     caractere = chr( convert_text  )
-        # elif convert_text < 0:
-        #     caractere = chr( convert_text + 255  )
-        # elif convert_text > 255:
-        #     caractere = chr( convert_text - 255 )
-        # r += caractere
-        # print( 'Ordem: {} - Ordinal: {} - Caractere Original: {} - Formula ( 22 * ({}+1): {} - Caractere Descriptografado: {}'
-            # .format( i, ord(c), c, i,  int(22 * (i+1)), caractere))
-        if convert_text <= 0:
-            print( convert_text + 255 )
-        elif convert_text >= 255:
-            print( convert_text - 255)
-    # return r       
+        convert_text = ord( c ) - ( 22 * ( i + 1 ) )
+        if convert_text >= 0 and convert_text <= 255:
+            caractere = chr( convert_text  )
+        elif convert_text < 0:
+            caractere = chr( convert_text + 255  )
+        elif convert_text > 255:
+            caractere = chr( convert_text - 255 )
+        r += caractere
+        print( 'Ordem: {} - Ordinal: {} - Caractere Original: {} - Formula ( 22 * ({}+1): {} - Caractere Descriptografado: {}'
+            .format( i, ord(c), c, i,  int(22 * (i+1)), caractere))
+    return r       
 
 
 # CAMPO C_CODIGO = 00002. C_CLIENTE = CARLOS JOSE ALVES FEITOSA
@@ -32,4 +24,4 @@ word = 'Ym¤½×ºú/7(_ ¥É¬èý8Icg\r´Êàö"8Ndz¦¼Òèþ'
 
 word2 = 'lm³Òãü+@(l¥É¬õ6?cF\r´Êàö"8Ndz¦¼Òèþ'
 
-descript(word)
+print( descript(word) )
