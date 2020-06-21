1036b42f3db2ee7e96d1586deefff7042f71023d
ISAIASRAMALHO <isaiasramalhoepai@gmail.com>
2020-06-20 23:20:25 -0300

    trying

--- teste_Desc.py
+++ teste_Desc.py
@@ -4,7 +4,7 @@
 
 
 def descript( v ):
-    # PARTICIONANDO O PARAMENTRO "v"
+    # PARTICIONANDO O PARAMETRO "v"
     particao = []
     cont = 0
     tam = len( v )
@@ -13,14 +13,14 @@ def descript( v ):
     n = 0
     convert_text = 0
     # ARREDONDANDO...
-    if tam % 8 > 0:
-        d = int( tam / 8 ) + 1
+    if tam % 10 > 0:
+        d = int( tam / 10 ) + 1
     else:
-        d = int( tam / 8 )
+        d = int( tam / 10 )
     # PARTICIONANDO
     for q in range( 0, d):
-        particao.append( v[ cont:cont+8] )
-        cont += 8
+        particao.append( v[ cont:cont+10] )
+        cont += 10
         
         # DESCRIPTOGRAFANDO POR PARTES.
         for i, c in enumerate( particao[q] ):
@@ -31,13 +31,15 @@ def descript( v ):
                     convert_text = ord( it  ) - ( 22 * ( i +1 ) )
                 caractere = ( chr( convert_text ) )
                 r += caractere
-            print( 'Ordem: {}, Código ASCII Original: {}, Fórmula ( 22 * ( {} + 1 ) ): {}, Novo Código ASCII: {}'.format( i, it, i, convert_text, caractere))
+            print( 'Ordem: {}, Código e Caractere ASCII Original: {} - {}, Fórmula ( 22 * ( {} + 1 ) ) = : {}, Novo Codigo e Caractere ASCII: {} - {}'
+                    .format( i, ord(it), it, i, (22*(i+1)), ord( caractere ), caractere))
+            print( '-'*80)
     return r       
 
 
 
 
 # CAMPO C_CODIGO = 00002. C_CLIENTE = CARLOS JOSE ALVES FEITOSA
-word = 'Ym¤½×ºú/8(_ ¥É¬èý8Icg\r´Êàö"8Ndz¦¼Òèþ'
+word = 'Ym¤½×ºú/10(_ ¥É¬èý8Icg\r´Êàö"8Ndz¦¼Òèþ'
 
 print( descript(word ) )
